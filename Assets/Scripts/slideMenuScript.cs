32980c6c6c241f253682f66bfb0a352d406f80e0
Desktop-PC Windows 10 Ferdinand Schlatt <fschlatt@gmail.com>
2018-06-10 21:41:10 +0200

    initial commit to branch, created slide menu and favorite buttons

---
+++ Assets/Scripts/slideMenuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slideMenuScript : MonoBehaviour
+{
+
+    //refrence for the pause menu panel in the hierarchy
+    public GameObject SlidePanel;
+    //animator reference
+    private Animator anim;
+    //variable for checking if the game is paused 
+
+    void Start()
+    {
+        //unpause the game on start
+        Time.timeScale = 1;
+        //get the animator component
+        anim = SlidePanel.GetComponent<Animator>();
+        //disable it on start to stop it from playing the default animation
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+
+    //function to pause the game
+    public void openMenu()
+    {
+        //enable the animator component
+        anim.enabled = true;
+        //play the Slidein animation
+        anim.Play("slideMenuIn");
+        //freeze the timescale
+        Time.timeScale = 0;
+    }
+    //function to unpause the game
+    public void closeMenu()
+    {
+        //play the SlideOut animation
+        anim.Play("slideMenuOut");
+        //set back the time scale to normal time scale
+        Time.timeScale = 1;
+    }
+
+}
