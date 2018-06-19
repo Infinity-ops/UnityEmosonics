39c4e6f3f936bc9531007181efe12a76f92ecd79
Desktop-PC Windows 10 Ferdinand Schlatt <fschlatt@gmail.com>
2018-06-19 19:38:53 +0200

    fixed scaling for landscape mode

---
+++ Assets/Scripts/changeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeColor : MonoBehaviour {
+
+    private Image image;
+
+	public GameObject colorWheel;
+ 
+    // Use this for initialization
+    void Start () {
+        image = GetComponent<Image>();
+    }
+
+    public void ButtonTransitionColors() {
+		GetComponent<Image>().color = new Color(Random.Range(0F, 1F), Random.Range(0F, 1F), Random.Range(0F, 1F));
+    }
+
+}
