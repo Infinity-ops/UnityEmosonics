76184eb63b285ed980d412a4f5a172e192e68923
narmada.ambika@gmail.com <narmada.ambika@gmail.com>
2018-07-13 13:44:20 +0200

    Added score / attempts

--- Assets/Scripts/DestroyObject.cs
+++ Assets/Scripts/DestroyObject.cs
@@ -2,37 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyObject : MonoBehaviour {
-  GameObject player;
-
+public class DestroyObject : MonoBehaviour
+{
+    GameObject player;
+    private GameObject StonePrefab;
+    private GameObject bc;
+   
     IEnumerator OnTriggerEnter(Collider collider)
     {
         if (!player || (player && collider.gameObject.tag == "player"))
         {
             PdAPI pd = GameObject.Find("PureData").GetComponent<PdAPI>();
-<<<<<<< HEAD
             double[] pos = new double[] { 0.1, 0.5 };
-<<<<<<< HEAD
             Destroy(this.gameObject);
             StonePrefab = GameObject.FindGameObjectWithTag("Projectile");
             DestroyObject(StonePrefab);
-=======
->>>>>>> 60596e6ea1e239616627d639dff7ab479f96c2a4
-=======
-            double[] pos = new double[] { 0.7, 0.5 };
->>>>>>> parent of 8b3bb35... Added score /Attempts
             pd.changeValue(pos);
+            GameCount.scoreValue += 10;
             pd.playAudio();
-
-            //AudioSource audio = GetComponent<AudioSource>();
-            //audio.Play();
-
+            Debug.Log(collider.gameObject);
             yield return new WaitForSeconds(0.7f);
-            Destroy(this.gameObject);
-            //Destroy(GameObject.Find("DragHandle"));
-           //player.GetComponent<Collider>().enabled = false;
-          
-           
         }
+       
+        
     }
 }
