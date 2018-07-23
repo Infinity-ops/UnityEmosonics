2d5d9631218911fd2211c2cf46003025f230d943
narmada.ambika@gmail.com <narmada.ambika@gmail.com>
2018-07-23 11:07:48 +0200

    CrossHairAdded in th game circle

--- Assets/Scripts/DragHandle.cs
+++ Assets/Scripts/DragHandle.cs
@@ -2,7 +2,8 @@
                                                                                                                                                                     //original
 public class DragHandle : MonoBehaviour
 {
-   private Vector3 _offset;
+  
+    private Vector3 _offset;
 
    private Vector3 _defaulPos;
 
@@ -23,42 +24,49 @@ public class DragHandle : MonoBehaviour
     {
        sphereCollider.radius = 5.0f;
     }
-   private void OnMouseDown()
-   {
-        sphereCollider.radius = 5.0f;
-        _offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint
-      (
-         new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z)
-      );
+    private void OnMouseDown()
+    {
+            sphereCollider.radius = 5.0f;
+            _offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint
+          (
+             new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z)
+          );
+
+            Cursor.visible = true;
        
-      Cursor.visible = true;
-   }
+    }
 
-   private void OnMouseDrag()
-   {
-        sphereCollider.radius = 5.0f;
+    private void OnMouseDrag()
+    {
+          sphereCollider.isTrigger = true;
+          //pt = gameObject.AddComponent<ProjectileTrajectory>();
 
-        var currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-       //Debug.Log("off" +_offset);
-    // Debug.Log("cscreepo" +currentScreenPoint);
-        _currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + _offset;
-     //  Debug.Log("cpos" +_currentPosition);
+          sphereCollider.radius = 5.0f;
 
-        transform.position = _currentPosition;
-   }
+            var currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            //Debug.Log("off" +_offset);
+            // Debug.Log("cscreepo" +currentScreenPoint);
+            _currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + _offset;
+            //  Debug.Log("cpos" +_currentPosition);
 
-   private void OnMouseUp()
-   {
-      Cursor.visible = true;
+            transform.position = _currentPosition;
+
+    }
+
+    private void OnMouseUp()
+    {
         
-      if (OnDragHandleReleaseEvent != null)
-      {
-         OnDragHandleReleaseEvent.Invoke();
+            Cursor.visible = true;
+
+            if (OnDragHandleReleaseEvent != null)
+            {
+                OnDragHandleReleaseEvent.Invoke();
+                sphereCollider.radius = 0.5f;
+            }
+
+            transform.position = _defaulPos;
             sphereCollider.radius = 0.5f;
         }
-        
-        transform.position = _defaulPos;
-        sphereCollider.radius = 0.5f;
-    }
+   
    
 }
--- Assets/Scripts/ProjectileTrajectory.cs
+++ Assets/Scripts/ProjectileTrajectory.cs
@@ -8,52 +8,62 @@ public class ProjectileTrajectory : MonoBehaviour
    public int PointNumber;
    public float InitialPointSize = 0.5f;
    public List<Transform> PointsList;
-
-   private Vector3 _updatedPosition = new Vector3(0, 0, 0);
+    public GameObject DragHandle;
+    public SphereCollider sphereCollider1;
+    private Vector3 _updatedPosition = new Vector3(0, 0, 0);
+    bool activeSelf;
 
    public void CreatePoints()
    {
-      PointsList = new List<Transform>();
+       
+        
+           // Debug.Log("Nari4");
+            PointsList = new List<Transform>();
 
-      for (var i = 0; i < PointNumber/1.75f; i++)
-      {
-         var _point = Instantiate(PointPrefab, transform.position, Quaternion.identity).transform;
-         _point.localScale = new Vector3
-         (
-            InitialPointSize / (i + 1) + (InitialPointSize / 2),
-            InitialPointSize / (i + 1) + (InitialPointSize / 2),
-            InitialPointSize / (i + 1) + (InitialPointSize / 2)
-         );
-           
-            _point.transform.parent = transform;
-         
-          //Debug.Log(transform.position);
-            PointsList.Add(_point);
-      }
+            for (var i = 0; i < PointNumber / 1.75f; i++)
+            {
+                var _point = Instantiate(PointPrefab, transform.position, Quaternion.identity).transform;
+                _point.localScale = new Vector3
+                (
+                   InitialPointSize / (i + 1) + (InitialPointSize / 2),
+                   InitialPointSize / (i + 1) + (InitialPointSize / 2),
+                   InitialPointSize / (i + 1) + (InitialPointSize / 2)
+                );
+
+                _point.transform.parent = transform;
+
+                //Debug.Log(transform.position);
+                PointsList.Add(_point);
+            }
+        
    }
 
-   public void UpdatePoints()
-   {
-      if (Input.GetMouseButton(0) && Slingshot.GetAngle() > 0 && Slingshot.GetAngle() <= 179.9f)
-      {
-         for (var i = 0; i < PointsList.Count; i++)
-         {
-            _updatedPosition.x = Slingshot.ReleasePointTransform.transform.position.x + i * (Slingshot.GetDistance(Slingshot.GetVelocity()) / PointNumber);
-            _updatedPosition.y = Slingshot.ReleasePointTransform.transform.position.y + Slingshot.GetHeight(Slingshot.GetVelocity(), PointNumber, i);
-            _updatedPosition.z = 0 ;
+    public void UpdatePoints()
+    {
+        if (sphereCollider1.isTrigger)
+        {
+            Debug.Log("Nari5");
+            if (Input.GetMouseButton(0) && Slingshot.GetAngle() > 0 && Slingshot.GetAngle() <= 179.9f)
+            {
+                for (var i = 0; i < PointsList.Count; i++)
+                {
+                    _updatedPosition.x = Slingshot.ReleasePointTransform.transform.position.x + i * (Slingshot.GetDistance(Slingshot.GetVelocity()) / PointNumber);
+                    _updatedPosition.y = Slingshot.ReleasePointTransform.transform.position.y + Slingshot.GetHeight(Slingshot.GetVelocity(), PointNumber, i);
+                    _updatedPosition.z = 0;
 
-            PointsList[i].transform.position = _updatedPosition;
-            PointsList[i].gameObject.SetActive(true);
-         }
-      }
-      else
-      {
-         for (var i = 0; i < PointsList.Count; i++)
-         {
-            PointsList[i].gameObject.SetActive(false);
-         }
-      }
-   }
+                    PointsList[i].transform.position = _updatedPosition;
+                    PointsList[i].gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                for (var i = 0; i < PointsList.Count; i++)
+                {
+                    PointsList[i].gameObject.SetActive(false);
+                }
+            }
+        }
+    }
 
    private void Start()
    {
@@ -62,6 +72,7 @@ public class ProjectileTrajectory : MonoBehaviour
 
    private void Update()
    {
+     
       UpdatePoints();
    }
 }
