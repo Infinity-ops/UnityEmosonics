2422c5314485da5353802e26bbdc55b0bf092b21
jnowainski <jnowainski@techfak.uni-bielefeld.de>
2018-07-31 15:33:24 +0200

    changed random sound to sample a circle coordinate directly now

--- Assets/Scripts/RandomSound.cs
+++ Assets/Scripts/RandomSound.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class RandomSound : MonoBehaviour {
-   // private PdAPI pd;
+    // private PdAPI pd;
+    private double[] pos;
+    private float radius_range = 1.0f;
     // Use this for initialization
     void Start () {
        
@@ -15,8 +17,23 @@ public class RandomSound : MonoBehaviour {
     }
    public void play() {
         PdAPI pd = GameObject.Find("PureData").GetComponent<PdAPI>();
-        double[] pos = new double[] { Random.Range(-1, 1), Random.Range(-1, 1) };
-        pd.changeValue(pos);
+        double[] random_pos = get_random_pos();
+        pd.changeValue(random_pos);
         pd.playAudio();
+    }
+
+   public double[] get_random_pos()
+    {
+        //create random radian and angle
+        float r = radius_range * Convert.ToSingle(Math.Sqrt(UnityEngine.Random.Range(0.0f, 1.0f)));
+        float theta = UnityEngine.Random.Range(0.0f, 1.0f) * 2.0f * Convert.ToSingle(Math.PI);
+
+        //calculate random x,y coordinate
+        double x = Math.Cos(theta) * r;
+        double y = Math.Sin(theta) * r;
+        pos = new double[] { x, y };
+
+        return pos;
+
     }
 }
