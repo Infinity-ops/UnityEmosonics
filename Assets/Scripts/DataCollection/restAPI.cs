0cc6ffef2b6a290eaca75c7573a72c191da5851a
Ferdinand Schlatt <fschlatt@gmail.com>
2018-09-18 11:54:09 +0200

    add documentation to restAPI

--- Assets/Scripts/DataCollection/restAPI.cs
+++ Assets/Scripts/DataCollection/restAPI.cs
@@ -50,6 +50,15 @@ public class restAPI : MonoBehaviour {
 
 	public static void SendUsage(double x, double y, bool doubleClick){
 
+		/**
+   		* Sends coordinates and boolean toggle of a double click or 
+		* not to data collection server
+		*
+   		* @param x x coordinate of click in unit circle
+		* @param y y coordinate of click in unit circle
+		* @param doubleClick False if play Button was pressed, True otherwise
+   		*/
+
 		string database = "commTool";
 
 		string datetime = System.DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
@@ -67,6 +76,15 @@ public class restAPI : MonoBehaviour {
 	}
 
 	public static void SendFavorite(double x, double y, int favorite_id) {
+
+		/**
+   		* Sends x and y coordinates of a favorite and it's ID when
+		* clicked to the data collection server
+		*
+   		* @param x x coordinate of favorite location in unit circle
+		* @param y y coordinate of favorite location in unit circle
+		* @param favorite_id number of favorite between 1 and 4
+   		*/
 
 		string database = "favorites";
 
@@ -86,6 +104,14 @@ public class restAPI : MonoBehaviour {
 
 	public static void SendSetting(string representation) {
 
+		/**
+   		* Sends visualization and representation setting data
+		* to data collection server
+		*
+   		* @param visualization 0 if russel or 1 if unit distribution of anchors is used
+		* @param representation 0 if points or 1 if text representation of anchors is used
+   		*/
+
 		string database = "settings";
 
 		string datetime = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
@@ -101,6 +127,19 @@ public class restAPI : MonoBehaviour {
 	}
 
 	public static void SendGame(double target_x, double target_y, int level, int num_other_targets, double shot_x, double shot_y, bool success, int tries_left) {
+
+		/**
+   		* Sends game shot data to data collection server
+		*
+   		* @param target_x x coordinate of target sound
+   		* @param target_y y coordinate of target sound
+		* @param level current level/difficulty
+		* @param num_other_targets number of other targets on board
+		* @param shot_x x coordinate of collision of shot with board
+		* @param shot_y y coordinate of collision of shot with board
+		* @param success True if correct target was hit, False otherwise
+		* @param tries_left number of tries player has left before game over
+   		*/
 
 		string database = "game";
 
