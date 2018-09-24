275c2bdb54d89dd1611079f28b9b2b0097a15e12
Ferdinand Schlatt <fschlatt@gmail.com>
2018-09-24 14:04:24 +0200

    doxygen extension fix

--- Assets/Editor/Doxygen/DoxygenWindow.cs
+++ Assets/Editor/Doxygen/DoxygenWindow.cs
@@ -37,8 +37,8 @@ public class DoxygenWindow : EditorWindow
 {
 	public static DoxygenWindow Instance;
 	public enum WindowModes{Generate,Configuration,About}
-	public string UnityProjectID = PlayerSettings.productName+":";
-	public string AssestsFolder = Application.dataPath;
+	public string UnityProjectID = "";
+	public string AssestsFolder = "";
 	public string[] Themes = new string[3] {"Default", "Dark and Colorful", "Light and Clean"};
 	public int SelectedTheme = 1;
 	WindowModes DisplayMode = WindowModes.Generate;
@@ -306,6 +306,8 @@ public class DoxygenWindow : EditorWindow
 
 	public void readBaseConfig()
 	{
+		UnityProjectID = PlayerSettings.productName+":";
+		AssestsFolder = Application.dataPath;
 		basefile = (TextAsset)Resources.Load("BaseDoxyfile", typeof(TextAsset));
 		reader = new StringReader(basefile.text);
 		if ( reader == null )
